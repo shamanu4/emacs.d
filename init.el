1ef313e260b5c6ac19decc86debadcd731bf19a1
Dmytro Kostochko <alerion.um@gmail.com>
2013-08-27 12:14:06 +0300

    update init.el: add undo key shortcut and python code navigation shortcuts

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
 (add-to-list 'load-path "~/.emacs.d")
 
+(when (require 'redo nil 'noerror)
+    (global-set-key (kbd "C-S-z") 'redo))
+
+(global-set-key (kbd "C-z") 'undo)
+
+(if (fboundp 'pc-selection-mode)
+        (pc-selection-mode)
+      (require 'pc-select))
+
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
@@ -15,6 +24,30 @@
            (list buffer-file-name)))
    (add-to-list 'flymake-allowed-file-name-masks
                 '("^[^\*]+\\.py$" flymake-pylint-init)))
+
+(defun py-to-start-of-class()
+  (interactive)
+  (py-beginning-of-def-or-class 'class)
+)
+
+(defun py-to-end-of-class()
+  (interactive)
+  (py-end-of-def-or-class 'class)
+)
+
+(add-hook 'python-mode-hook
+               '(lambda ()
+  (local-set-key [(s menu)] 'rope-code-assist)
+  (local-set-key [(s up)] 'python-move-to-start-of-class)
+  (local-set-key [(s down)] 'python-move-to-end-of-class)
+  (local-set-key [(meta down)] 'py-end-of-def-or-class)
+  (local-set-key [(meta up)] 'py-beginning-of-def-or-class)
+  (local-set-key (kbd "C-c C-a") 'py-to-start-of-class)
+  (local-set-key (kbd "C-c C-e") 'py-to-end-of-class)
+  (local-set-key (kbd "s-q") 'py-shift-region-left)
+  (local-set-key (kbd "s-w") 'py-shift-region-right)
+  )
+)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
